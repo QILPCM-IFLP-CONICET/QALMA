59e040cebddec5c50ebb518e0cf8e10ce009576f
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2025-09-19 09:50:46 -0300

    more debug

--- qalma/operators/arithmetic.py
+++ qalma/operators/arithmetic.py
@@ -163,12 +163,12 @@ class SumOperator(Operator):
             # Here we assume that after simplify, the operator is a single term
             # (not a SumOperator), a OneBodyOperator, or a sum of a one-body operator
             # and terms acting over an specific block.
-            logging.debug(" aggressive approach for SumOperator.isherm")
+            print(" aggressive approach for SumOperator.isherm")
             nh_sum = SumOperator(non_hermitian_tuple, self.system).simplify()
             if not hasattr(nh_sum, "terms"):
                 self._isherm = nh_sum.isherm
                 if self._isherm is False:
-                    logging.debug(f"  the unique nh term {nh_sum} is not hermitician.")
+                    print(f"  the unique nh term {nh_sum} is not hermitician.")
                 return self._isherm
             # Hermitician until the opposite is shown:
             isherm = True
@@ -183,9 +183,9 @@ class SumOperator(Operator):
                     nh_size = abs((ah_part * ah_part).tr())
                     term_isherm = nh_size < QALMA_TOLERANCE
                     if term_isherm is False:
-                        logging.debug(f"  {term_isherm}  too large for hermitician")
+                        print(f"  {term_isherm}  too large for hermitician")
                 if not term_isherm:
-                    logging.debug(f"   {term}\n    is not hermitician")
+                    print(f"   {term}\n    is not hermitician")
                     isherm = False
                     break
             self._isherm = isherm
@@ -203,7 +203,7 @@ class SumOperator(Operator):
             return True
 
         if isherm is False:
-            logging.debug("   was previously established that is not hermitician.")
+            print("   was previously established that is not hermitician.")
         return bool(self._isherm)
 
     @property
