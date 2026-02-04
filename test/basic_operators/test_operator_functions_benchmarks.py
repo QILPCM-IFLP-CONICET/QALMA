1202541df7ec70a64a26afbfa05064ed2a4cec4c
Mauricio Matera <matera@fisica.unlp.edu.ar>
2026-02-03 23:17:59 -0300

    fix conventions

--- test/basic_operators/test_operator_functions_benchmarks.py
+++ test/basic_operators/test_operator_functions_benchmarks.py
@@ -7,8 +7,8 @@ from test.helper import HAMILTONIAN, SX_A, SX_TOTAL, SZ_TOTAL, TEST_CASES_STATES
 
 import pytest
 
-from qalma.operators.functions import commutator, fidelity
 from qalma.operators import ProductOperator
+from qalma.operators.functions import commutator, fidelity
 
 
 @pytest.mark.skipif(
@@ -110,7 +110,7 @@ def test_fidelity(benchmark, name_rho, name_sigma):
 
     rho = TEST_CASES_STATES[name_rho]
     sigma = TEST_CASES_STATES[name_sigma]
-    if not (isinstance(rho, ProductOperator) and  isinstance(sigma, ProductOperator)):
+    if not (isinstance(rho, ProductOperator) and isinstance(sigma, ProductOperator)):
         return
 
     def impl():
