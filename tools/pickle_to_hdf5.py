16e6f6e9c12f1a7433bee5da7b959aa78608718c
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2026-02-21 14:57:41 -0300

    black

--- tools/pickle_to_hdf5.py
+++ tools/pickle_to_hdf5.py
@@ -5,9 +5,9 @@ import sys
 
 for filename in sys.argv[1:]:
     if filename[-4:] == ".pkl":
-    	out_filename = filename[:-4] + ".h5"
+        out_filename = filename[:-4] + ".h5"
     else:
-    	out_filename = filename + ".h5"
+        out_filename = filename + ".h5"
 
     print(filename, "->", out_filename)
     with open(filename, "rb") as f:
