26e5f1db80523b8273a18401a2299cc2029fac31
mmatera <mmatera@fisica.unlp.edu.ar>
2026-03-27 15:58:39 -0300

    _isherm instead of isherm

--- test/graphic_profile.sh
+++ test/graphic_profile.sh
@@ -8,10 +8,10 @@ PROFILE_BRANCH=`echo prof/$BRANCH/$1.prof`
 
 echo "graphic profile from '$PROFILE_MASTER'"
 echo "graphic profile from '$PROFILE_BRANCH'"
-#gprof2dot --color-nodes-by-selftime -f pstats "$PROFILE_MASTER"  |dot -Tsvg -o gprof_master.svg
-#gprof2dot --color-nodes-by-selftime -f pstats "$PROFILE_BRANCH"  |dot -Tsvg -o gprof_$BRANCH.svg
-gprof2dot -f pstats "$PROFILE_MASTER"  |dot -Tsvg -o gprof_master.svg
-gprof2dot -f pstats "$PROFILE_BRANCH"  |dot -Tsvg -o gprof_$BRANCH.svg
+gprof2dot --color-nodes-by-selftime -f pstats "$PROFILE_MASTER"  |dot -Tsvg -o gprof_master.svg
+gprof2dot --color-nodes-by-selftime -f pstats "$PROFILE_BRANCH"  |dot -Tsvg -o gprof_$BRANCH.svg
+# gprof2dot -f pstats "$PROFILE_MASTER"  |dot -Tsvg -o gprof_master.svg
+# gprof2dot -f pstats "$PROFILE_BRANCH"  |dot -Tsvg -o gprof_$BRANCH.svg
 
 
 echo "graphic profiles in  gprof_master.svg and  gprof_$BRANCH.svg"
