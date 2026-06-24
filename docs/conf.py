100a18af1e14b99cf4e46968233ae136ff93e08a
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2026-06-24 09:20:30 -0300

    black

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "nbsphinx",
     "myst_parser",
-    "sphinx.ext.imgconverter", # Automatically handles image format conversions
+    "sphinx.ext.imgconverter",  # Automatically handles image format conversions
 ]
 
 templates_path = ["_templates"]
@@ -34,10 +34,6 @@ source_suffix = {
 }
 master_doc = "index"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
-
-# Options for LaTeX output ---------------------------
-
-latex_logo = "logo.pdf"
 
 # -- Options for HTML output -------------------------------------------------
 
